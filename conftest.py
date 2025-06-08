283b175291cb68dc8ec7560fd35902fb50f1aa86
Anton <annnt9na@yandex.ru>
2025-06-08 22:16:01 +0300

    Updated conftets.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,18 @@
 import pytest
+from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 
 
 @pytest.fixture(scope='function')
 def browser():
     """Фикстура для запуска браузера"""
-    browser = webdriver.Chrome()
+
+    options = Options()
+    options.add_argument('--headless')  # Без этого на CI не запустится
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+
+    browser = webdriver.Chrome(options=options)
     browser.implicitly_wait(5)  # ждёт до 5 секунд при поиске каждого элемента
     yield browser
     browser.quit()
